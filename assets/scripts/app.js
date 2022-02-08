095bf814efc05287f2f1d8a6d3300c1bf30070b7
Alex <alexhelloworld@protonmail.com>
2022-02-08 12:02:04 +0700

    backdrop and cancel

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,25 @@
 const addBtn =document.getElementById('addBtn')
 const modal = document.getElementById('add-modal')
 const backdrop = document.getElementById('backdrop')
+const btnCancel = modal.querySelector('.btn--passive')
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible')
+}
 
 const toggleMovieModal = () => {
    modal.classList.toggle('visible')
-   backdrop.classList.toggle('visible')
+   toggleBackdrop();
+}
+
+const backdropClickHandler = () => {
+    toggleMovieModal()
+}
+
+const cancelClickHandler = () => {
+    toggleMovieModal();
 }
 
 addBtn.addEventListener('click', toggleMovieModal )
-backdrop.addEventListener('click', toggleMovieModal)
+backdrop.addEventListener('click', backdropClickHandler)
+btnCancel.addEventListener('click', cancelClickHandler)
