c8dff415a2c541bb280d96b4da2ef63d52b38489
Alex <alexhelloworld@protonmail.com>
2022-02-08 12:15:13 +0700

    form handler + validation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@ const addBtn =document.getElementById('addBtn')
 const modal = document.getElementById('add-modal')
 const backdrop = document.getElementById('backdrop')
 const btnCancel = modal.querySelector('.btn--passive')
+const btnConfirm = btnCancel.nextElementSibling
+const inputs = modal.querySelectorAll('input')
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible')
@@ -20,6 +22,17 @@ const cancelClickHandler = () => {
     toggleMovieModal();
 }
 
+const addMovieHandler = () => {
+    const titleValue = inputs[0].value
+    const imageValue = inputs[1].value
+    const ratingValue = inputs[2].value
+
+    if (titleValue.trim() === '' || imageValue.trim() === '' || ratingValue.trim() === '' || +ratingValue < 1 || +ratingValue > 5) {
+        alert('Please input valid values!')
+    }
+}
+
 addBtn.addEventListener('click', toggleMovieModal )
 backdrop.addEventListener('click', backdropClickHandler)
 btnCancel.addEventListener('click', cancelClickHandler)
+btnConfirm.addEventListener('click',  addMovieHandler)
